94a1d3f6457834e1e371abbea889513bcc871820
Radek Tesař <radek.tesar135@gmail.com>
2024-03-07 12:17:34 +0100

    Fond and color definition

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ root.minsize(400,500)
 root.iconbitmap("icon.ico")
 root.resizable(False, False)
 
+# Font and color definition
+main_font = ("Times New Roman", 12)
+main_color = "#dd7f00"
+button_color = "#e2cff4"
+root.config(bg=main_color)
+
 # Main cycle
 root.mainloop()
