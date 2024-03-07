6012f69170d4bb457cfab71766874876329a1999
Radek Tesař <radek.tesar135@gmail.com>
2024-03-07 12:32:08 +0100

    Added first frame (Input Frame)

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ from tkinter import *
 # Main window
 root = Tk()
 root.title("Seznam úkolů")
-root.minsize(400,500)
+root.minsize(600,400)
 root.iconbitmap("icon.ico")
 root.resizable(False, False)
 
 # Font and color definition
 main_font = ("Times New Roman", 12)
 main_color = "#dd7f00"
-button_color = "#e2cff4"
+button_color = "#ffbe66"
 root.config(bg=main_color)
 
 # Frames
-input_frame = Frame(root, bg="black")
+input_frame = Frame(root, bg="white")
 input_frame.pack()
 text_frame = Frame(root, bg="gray")
 text_frame.pack()
 button_frame = Frame(root, bg="gray")
 button_frame.pack()
+
+# Input frame
+user_input = Entry(input_frame, width=30, borderwidth=3, font=main_font)
+user_input.grid(row=0, column=0)
+add_button = Button(input_frame, text="Přidat", borderwidth=2, font=main_font, bg=button_color)
+add_button.grid(row=0, column=1)
 
 # Main cycle
 root.mainloop()
