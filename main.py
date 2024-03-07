9c0bd610ede8fd20692c47a958af1df7b2ad73f5
Radek Tesař <radek.tesar135@gmail.com>
2024-03-07 12:15:23 +0100

    Created Main window and mainloop

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# Main window
+root = Tk()
+root.title("Seznam úkolů")
+root.minsize(400,500)
+root.iconbitmap("icon.ico")
+root.resizable(False, False)
+
+# Main cycle
+root.mainloop()
