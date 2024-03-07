ade4bbe0d4b08947282d1103ae0f6424082399f7
Radek Tesař <radek.tesar135@gmail.com>
2024-03-07 12:24:32 +0100

    Added frames into root

--- main.py
+++ main.py
@@ -13,5 +13,13 @@ main_color = "#dd7f00"
 button_color = "#e2cff4"
 root.config(bg=main_color)
 
+# Frames
+input_frame = Frame(root, bg="black")
+input_frame.pack()
+text_frame = Frame(root, bg="gray")
+text_frame.pack()
+button_frame = Frame(root, bg="gray")
+button_frame.pack()
+
 # Main cycle
 root.mainloop()
